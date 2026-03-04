247a0403ea1a91328983afb7ecf556b5de4899d2
Oli-lab-nun <maxiangyu_nun@163.com>
2026-03-04 13:18:47 +0800

    Add files via upload

---
+++ LLaMA-Factory/train.sh
@@ -0,0 +1,2 @@
+DISABLE_VERSION_CHECK=1 ASCEND_RT_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 FORCE_TORCHRUN=1 llamafactory-cli train /data/mxy/workspace/L-MTP-main/configs_pangu/pangu.yaml
+# DISABLE_VERSION_CHECK=1 ASCEND_RT_VISIBLE_DEVICES=4,5,6,7 FORCE_TORCHRUN=1 llamafactory-cli train /data/mxy/workspace/L-MTP-main/configs_pangu/pangu_head.yaml
